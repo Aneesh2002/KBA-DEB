92b185b7dc19dd0e74b02ea4eba13d7387b27d56
Aneesh2002 <aneeshmn4@gmail.com>
2024-10-03 14:58:35 +0530

    edited

---
+++ Java Script/classwork/functions/function3/script.js
@@ -0,0 +1,25 @@
+//type conversion or coercion
+
+console.log("20"+5)
+console.log("20" -5)
+console.log("20"*5)
+console.log("20"/5)
+
+console.log(true+1)
+
+console.log(Number("42"))
+console.log(Number("hello"))
+
+console.log(typeof String(42))
+console.log(String(true))
+
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean("hello"))
+console.log(Boolean(1))
+console.log(Boolean(0))
+console.log(Boolean(''))
+console.log(Boolean("hello"))
+
+console.log(parseFloat("15.99"))
+console.log(parseInt("15.99"))
